200381e1ed5f2317a74f26626b6ea9901cee4dda
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2024-01-14 22:19:13 +0100

    Update examples.py

--- examples.py
+++ examples.py
@@ -5,6 +5,6 @@ def your_function(foo: str, bar: int):
    pass
 
 
-@async_force_type # when stacking decorators, force_type must be applied as last (bottom)
+@async_force_type # when stacking decorators, async_force_type must be applied as last (bottom)
 async def your_function(foo: str, bar: int):
    pass
