2412c5fc1fef145a711d8747c5f14149521df0e5
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2024-01-14 22:13:59 +0100

    Create force_types.py

---
+++ force_types.py
@@ -0,0 +1,31 @@
+import typing
+
+def force_type(func: typing.Callable):
+    def wrapper(*args, **kwargs):
+        annotations = func.__annotations__
+
+        kwargs_list = list(annotations.keys())
+        for i, arg in enumerate(args):
+            kwargs[kwargs_list[i]] = arg
+
+        for kwarg, value in kwargs.items():
+            if not isinstance(value, annotations[kwarg]):
+                raise TypeError(f'{value} is not of type {annotations[kwarg]}. Please submit parameters in their hinted type.')
+
+        return func(**kwargs)
+    return wrapper
+
+def async_force_type(func: typing.Coroutine):
+    async def wrapper(*args, **kwargs):
+        annotations = func.__annotations__
+
+        kwargs_list = list(annotations.keys())
+        for i, arg in enumerate(args):
+            kwargs[kwargs_list[i]] = arg
+
+        for kwarg, value in kwargs.items():
+            if not isinstance(value, annotations[kwarg]):
+                raise TypeError(f'{value} is not of type {annotations[kwarg]}. Please submit parameters in their hinted type.')
+
+        return await func(**kwargs)
+    return wrapper
