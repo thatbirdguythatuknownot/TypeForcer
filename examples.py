e955b1bd923b5275b54029504e284a8938c33765
DevBauke <baukelim@hotmail.nl>
2024-02-13 21:24:41 +0100

    GenericAlias iterator support

--- examples.py
+++ examples.py
@@ -1,13 +1,16 @@
 from force_types import *
+# force types supports ONE level of type hint nesting: -> list[str | int]
+# does NOT support: -> list[dict[str, int]] (TWO level hint nesting)
+
 
 @force_types # when stacking decorators, force_type must be applied as last (bottom)
-def your_function(foo: str, bar: int):
-   pass
+def your_function(foo: list[str | int], bar: int) -> None:
+    pass
 
 
 @async_force_types # when stacking decorators, async_force_type must be applied as last (bottom)
-async def your_function(foo: str, bar: int):
+async def async_your_function(foo: str, bar: int) -> None:
    pass
 
 
-f = your_function(2, bar='test') # will throw a TypeError
+f = your_function(['hi', 5.0], bar=5) # will throw a TypeError (due to invalid passed list (float instead of int))
--- force_types.py
+++ force_types.py
@@ -3,12 +3,29 @@ import typing
 def force_types(func: typing.Callable):
     def wrapper(*args, **kwargs):
         annotations = func.__annotations__
+
         for missing_kwarg, arg in zip(annotations.keys(), args):
             kwargs[missing_kwarg] = arg  
 
-        for kwarg, value in kwargs.items():
-            if not isinstance(value, annotations[kwarg]):
-                raise TypeError(f'{value} is not of type {annotations[kwarg]}. Please submit parameters in their hinted type.')
+        for kwarg, value in reversed(kwargs.items()):
+            annotation = annotations[kwarg]
+            annotation_origin = getattr(annotation, "__origin__", annotation)
+
+            if not isinstance(value, annotation_origin):
+                raise TypeError(f'{value} -> Invalid type {type(value)} for argument "{kwarg}" with hinted type {annotation}')
+
+            # GenericAlias support (nested hints): list[str | int], dict[int, int]
+            if isinstance(value, typing.Union[typing.Iterable, typing.AsyncIterable, typing.Iterator, typing.AsyncIterator]) and hasattr(annotation, '__args__'):
+                    raise_exception = False
+                    if isinstance(annotation.__origin__, type(dict)) and len(annotation.__args__) > 1:
+                        if len(list(filter(lambda x: isinstance(x, annotation.__args__[0]) and isinstance(value[x], annotation.__args__[1]), value))) < len(value):
+                            raise_exception = True
+                    else:
+                        if len(list(filter(lambda x: isinstance(x, annotation.__args__), value))) < len(value):
+                            raise_exception = True
+
+                    if raise_exception:
+                        raise TypeError(f'Invalid return type {type(value)} [{value}] should be of type {annotation}')
 
         return func(**kwargs)
     return wrapper
@@ -16,12 +33,29 @@ def force_types(func: typing.Callable):
 def async_force_types(func: typing.Coroutine):
     async def wrapper(*args, **kwargs):
         annotations = func.__annotations__
+
         for missing_kwarg, arg in zip(annotations.keys(), args):
             kwargs[missing_kwarg] = arg  
 
-        for kwarg, value in kwargs.items():
-            if not isinstance(value, annotations[kwarg]):
-                raise TypeError(f'{value} is not of type {annotations[kwarg]}. Please submit parameters in their hinted type.')
+        for kwarg, value in reversed(kwargs.items()):
+            annotation = annotations[kwarg]
+            annotation_origin = getattr(annotation, "__origin__", annotation)
+
+            if not isinstance(value, annotation_origin):
+                raise TypeError(f'{value} -> Invalid type {type(value)} for argument "{kwarg}" with hinted type {annotation}')
+
+            # GenericAlias support (nested hints): list[str | int], dict[int, int]
+            if isinstance(value, typing.Union[typing.Iterable, typing.AsyncIterable, typing.Iterator, typing.AsyncIterator]) and hasattr(annotation, '__args__'):
+                    raise_exception = False
+                    if isinstance(annotation.__origin__, type(dict)) and len(annotation.__args__) > 1:
+                        if len(list(filter(lambda x: isinstance(x, annotation.__args__[0]) and isinstance(value[x], annotation.__args__[1]), value))) < len(value):
+                            raise_exception = True
+                    else:
+                        if len(list(filter(lambda x: isinstance(x, annotation.__args__), value))) < len(value):
+                            raise_exception = True
+
+                    if raise_exception:
+                        raise TypeError(f'Invalid return type {type(value)} [{value}] should be of type {annotation}')
 
         return await func(**kwargs)
     return wrapper
