de628cf2e3b597e4d52298375504085763d7374e
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2024-02-15 05:24:54 +0800

    Improved type forcer

--- force_types.py
+++ force_types.py
@@ -1,113 +1,179 @@
+import inspect
+import types
 import typing
+from dataclasses import dataclass
+from itertools import chain
+
+def type_name(typ: type) -> str:
+    if hasattr(typ, "__qualname__"):
+        return typ.__qualname__
+    if hasattr(typ, "__name__"):
+        return typ.__name__
+    return repr(typ)
+
+@dataclass
+class Annotation:
+    annotation: object
+    idx: int | None = None
+    level: int | None = None
+
+    def __str__(self):
+        annotation_s = type_name(self.annotation)
+
+        if self.idx is None:
+            if self.level is None:
+                return annotation_s
+            return f"{annotation_s} (at level {self.level})"
+
+        if self.level is None:
+            return f"{annotation_s} (index {self.idx})"
+        return f"{annotation_s} (at level {self.level}, index {self.idx})"
+
+@dataclass
+class Failed:
+    value: object
+    annotation: object
+
+    def __str__(self):
+        value_typename = type_name(type(self.value))
+        return f"type: {value_typename} -> fails: {self.annotation} (from value: {self.value!r})"
+
+# returns [Failed(offending_value, annotation), ...] if it failed, otherwise None
+def recursive_check(value, annotation, level=0):
+    if annotation is None:
+        if value is None:
+            return None
+        return [Failed(value, Annotation(None, level=level))]
+
+    annotation_origin = getattr(annotation, "__origin__", annotation)
+    if not isinstance(value, annotation_origin):
+        return [Failed(value, Annotation(annotation, level=level))]
+
+    # check for things like str | int and typing.Union[str, int] and
+    # turn them into (str, int)
+    # also turns Optional[str] into (str, type(None))
+    if isinstance(annotation, (types.UnionType, typing._UnionGenericAlias)):
+        annotation = annotation.__args__
+    if isinstance(annotation, tuple):
+        for annot in annotation:
+            if not recursive_check(value, annot, level + 1):
+                return None
+        return [Failed(value, Annotation(annotation, level=level))]
+
+    fail = None
+    # GenericAlias support (nested hints): list[str | int], dict[int, int]
+    if isinstance(
+        value,
+        (
+            typing.Iterable,
+            typing.AsyncIterable,
+        )
+    ) and not isinstance(
+        value,
+        (
+            typing.Iterator,
+            typing.AsyncIterator,
+        )
+    ) and hasattr(annotation, "__args__") and isinstance(
+        annotation,
+        (
+            types.GenericAlias,
+            typing._GenericAlias,
+        )
+    ):
+        if ann_args := annotation.__args__:
+            if annotation_origin in (tuple, typing.Tuple):
+                if len(value) != len(ann_args):
+                    return [Failed(value, Annotation(annotation, level=level))]
+
+                for i, (x, typ) in enumerate(zip(value, ann_args)):
+                    if fail := recursive_check(x, typ, level + 1):
+                        fail.append(Failed(value, Annotation(annotation, i, level)))
+                        break
+
+            elif annotation_origin in (dict, typing.Dict):
+                tkey, tval = ann_args
+
+                for i, (key, val) in enumerate(value.items()):
+                    if fail := recursive_check(key, tkey, level + 1):
+                        fail.append(Failed(value, Annotation(annotation, i, level)))
+                        break
+                    if fail := recursive_check(val, tval, level + 1):
+                        fail.append(Failed(value, Annotation(annotation, i, level)))
+                        break
+
+            else:
+                if len(ann_args) == 1:
+                    ann_args, = ann_args
+
+                for i, x in enumerate(value):
+                    if fail := recursive_check(x, ann_args, level + 1):
+                        fail.append(Failed(value, Annotation(annotation, i, level)))
+                        break
+    return fail
 
 def force_types(func: typing.Callable):
+    sig = inspect.signature(func)
+    annotations = func.__annotations__
+    has_return = 'return' in annotations
+    if has_return:
+        return_annotation = annotations['return']
+
     def wrapper(*args, **kwargs):
-        annotations = func.__annotations__
+        bound = sig.bind(*args, **kwargs)
 
-        for missing_kwarg, arg in zip(annotations.keys(), args):
-            kwargs[missing_kwarg] = arg
+        for arg, value in chain(bound.arguments.items(),
+                                bound.kwargs):
+            if arg not in annotations:
+                continue
 
-        for kwarg, value in reversed(kwargs.items()):
-            annotation = annotations[kwarg]
-            annotation_origin = getattr(annotation, "__origin__", annotation)
+            if fail := recursive_check(value, annotations[arg]):
+                err = TypeError(f'argument "{arg}" type mismatch!')
+                err.add_note("recursive checker traceback (deepest layer first):")
+                for failure in fail:
+                    err.add_note(f"    {failure}")
+                raise err
 
-            if not isinstance(value, annotation_origin):
-                raise TypeError(
-                    f'{value} -> Invalid type {type(value)} for argument "{kwarg}" with hinted type {annotation}'
-                )
+        res = func(*args, **kwargs)
+        if has_return:
+            if fail := recursive_check(res, return_annotation):
+                err = TypeError(f'return type mismatch!')
+                err.add_note("recursive checker traceback (deepest layer first):")
+                for failure in fail:
+                    err.add_note(f"    {failure}")
 
-            # GenericAlias support (nested hints): list[str | int], dict[int, int]
-            if isinstance(
-                value,
-                typing.Union[
-                    typing.Iterable,
-                    typing.AsyncIterable,
-                ]
-            ) and hasattr(annotation, "__args__"):
-                raise_exception = False
-                if (
-                    isinstance(annotation.__origin__, type(dict))
-                    and len(annotation.__args__) > 1
-                ):
-                    if len(
-                        list(
-                            filter(
-                                lambda x: isinstance(x, annotation.__args__[0])
-                                and isinstance(value[x], annotation.__args__[1]),
-                                value,
-                            )
-                        )
-                    ) < len(value):
-                        raise_exception = True
-
-                else:
-                    if len(
-                        list(
-                            filter(lambda x: isinstance(x, annotation.__args__), value)
-                        )
-                    ) < len(value):
-                        raise_exception = True
-
-                if raise_exception:
-                    raise TypeError(
-                        f'{value} -> Invalid type {type(value)} for argument "{kwarg}" with hinted type {annotation}'
-                    )
-
-        return func(**kwargs)
+        return res
     return wrapper
 
-def async_force_types(func: typing.Coroutine):
+def async_force_types(func: typing.Callable):
+    sig = inspect.signature(func)
+    annotations = func.__annotations__
+    has_return = 'return' in annotations
+    if has_return:
+        return_annotation = annotations['return']
+
     async def wrapper(*args, **kwargs):
-        annotations = func.__annotations__
+        bound = sig.bind(*args, **kwargs)
 
-        for missing_kwarg, arg in zip(annotations.keys(), args):
-            kwargs[missing_kwarg] = arg
+        for arg, value in chain(bound.arguments.items(),
+                                bound.kwargs):
+            if arg not in annotations:
+                continue
 
-        for kwarg, value in reversed(kwargs.items()):
-            annotation = annotations[kwarg]
-            annotation_origin = getattr(annotation, "__origin__", annotation)
+            if fail := recursive_check(value, annotations[arg]):
+                err = TypeError(f'argument "{arg}" type mismatch!')
+                err.add_note("recursive checker traceback (deepest layer first):")
+                for failure in fail:
+                    err.add_note(f"    {failure}")
+                raise err
 
-            if not isinstance(value, annotation_origin):
-                raise TypeError(
-                    f'{value} -> Invalid type {type(value)} for argument "{kwarg}" with hinted type {annotation}'
-                )
+        res = await func(*args, **kwargs)
+        if has_return:
+            if fail := recursive_check(res, return_annotation):
+                err = TypeError(f'return type mismatch!')
+                err.add_note("recursive checker traceback (deepest layer first):")
+                for failure in fail:
+                    err.add_note(f"    {failure}")
 
-            # GenericAlias support (nested hints): list[str | int], dict[int, int]
-            if isinstance(
-                value,
-                typing.Union[
-                    typing.Iterable,
-                    typing.AsyncIterable,
-                ]
-            ) and hasattr(annotation, "__args__"):
-                raise_exception = False
-                if (
-                    isinstance(annotation.__origin__, type(dict))
-                    and len(annotation.__args__) > 1
-                ):
-                    if len(
-                        list(
-                            filter(
-                                lambda x: isinstance(x, annotation.__args__[0])
-                                and isinstance(value[x], annotation.__args__[1]),
-                                value,
-                            )
-                        )
-                    ) < len(value):
-                        raise_exception = True
-
-                else:
-                    if len(
-                        list(
-                            filter(lambda x: isinstance(x, annotation.__args__), value)
-                        )
-                    ) < len(value):
-                        raise_exception = True
-
-                if raise_exception:
-                    raise TypeError(
-                        f'{value} -> Invalid type {type(value)} for argument "{kwarg}" with hinted type {annotation}'
-                    )
-
-        return await func(**kwargs)
+        return res
     return wrapper
